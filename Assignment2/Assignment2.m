b71d489de7c47ae304c24702a028c8a92a9477d5
Andyteng <tengxiang0417@outlook.com>
2017-03-12 12:54:39 +0100

    modifications on Weak learner

--- Assignment2/Assignment2.m
+++ Assignment2/Assignment2.m
@@ -16,21 +16,31 @@ clear all; close all; clc
 % % legend('y1','y2');
 % ======================= Question 2: weak learner =======================
 
-% load data_x.mat
-% load data_y.mat
-% 
-% input1 = prdataset(a,labels);
-% [f_opt,thea_opt,y_opt] = stump(input1);
-% 
-% % used to test threshold
-% figure;
-% scatterd(input1,'legend');
-% hold on;
-% stem(thea_opt, 4,'b');
-% stem(thea_opt, -4, 'b');
-% title('Decision Stump');
-% xlabel('feature 1');
-% ylabel('feature 2');
+load data_x.mat
+load data_y.mat
+
+x = a;
+y = labels;
+
+for i = 1:size(a,1)
+    if y(i) ==2
+        y(i)=-1;
+    else 
+        y(i)=1;
+    end
+end
+input1 = prdataset(x,y);
+[f_opt,thea_opt,y_opt] = stump(input1);
+
+% used to test threshold
+figure;
+scatterd(input1,'legend');
+hold on;
+stem(thea_opt, 4,'b');
+stem(thea_opt, -4, 'b');
+title('Decision Stump');
+xlabel('feature 1');
+ylabel('feature 2');
 
 %% ===================== Question 3: Test with 'gendats' ==================
 % input2 = gendats([40 40], 2 ,2);
@@ -97,51 +107,52 @@ clear all; close all; clc
 
 %% Question e--adaboost
 
-load data_x.mat
-load data_y.mat
-data = load ('optdigitsubset.txt');
+% load data_x.mat
+% load data_y.mat
+% data = load ('optdigitsubset.txt');
+% 
+% % class0 = data((1:554),:);
+% % class1 = data((555:end),:);
+% % idx0 = randperm(size(class0,1),50);
+% % idx1 = randperm(size(class1,1),50);
+% % r0 = class0(idx0,:);
+% % r1 = class1(idx1,:);
+% % r = [r0;r1];
+% % y = [ones(50,1).*(-1);ones(50,1)];
+% % input_r = prdataset(r,y);
+% x = a;
+% y = labels;
+% for i = 1: length(y)
+%     if y(i) == 2
+%         y(i) = -1; 
+%     end
+% end
+% input1 = prdataset(x,y);
+% 
+% 
+% 
+% 
+% N = length(y); % X training labels
+% w = 1/N * ones(N,1); %Weights initialization
+% T = 3; % Number of iteration
+% for t=1:T
+%     p = w ./ sum(w);
+%     [f_opt,thea_opt,h] = stump(input1,p); % call weak learner
+%     error = sum(p.*abs(sign(h-y)));
+%     beta = error/(1-error);
+%     w = w.*(beta.^(ones(100,1)-abs(h-y)));
+%     
+%     % used to test threshold
+% figure;
+% scatterd(input1,'legend');
+% hold on;
+% stem(thea_opt, 4,'b');
+% stem(thea_opt, -4, 'b');
+% title('Decision Stump');
+% xlabel('feature 1');
+% ylabel('feature 2');
+% end
 
-% class0 = data((1:554),:);
-% class1 = data((555:end),:);
-% idx0 = randperm(size(class0,1),50);
-% idx1 = randperm(size(class1,1),50);
-% r0 = class0(idx0,:);
-% r1 = class1(idx1,:);
-% r = [r0;r1];
-% y = [ones(50,1).*(-1);ones(50,1)];
-% input_r = prdataset(r,y);
-x = a;
-y = labels;
-for i = 1: length(y)
-    if y(i) == 2
-        y(i) = -1; 
-    end
-end
-input1 = prdataset(x,y);
-
-
-
-
-N = length(y); % X training labels
-w = 1/N * ones(N,1); %Weights initialization
-T = 3; % Number of iteration
-for t=1:T
-    p = w ./ sum(w);
-    [f_opt,thea_opt,h] = stump(input1,p); % call weak learner
-    error = sum(p.*abs(sign(h-y)));
-    beta = error/(1-error);
-    w = w.*(beta.^(ones(100,1)-abs(h-y)));
-    
-    % used to test threshold
-figure;
-scatterd(input1,'legend');
-hold on;
-stem(thea_opt, 4,'b');
-stem(thea_opt, -4, 'b');
-title('Decision Stump');
-xlabel('feature 1');
-ylabel('feature 2');
-end
 
 
 
--- Assignment2/stump.m
+++ Assignment2/stump.m
@@ -1,15 +1,19 @@
-function [f_opt,thea_opt,h] = stump(X,p)
-x = X.data;
-y = X.labels;
-s = size(x);
-T = zeros(s(2),1);
+function [f_opt,thea_opt,y_opt] = stump(x)
+data = x.data;
+y = x.labels;
+s = size(data);
 
+
+
+% exaustive search for all features
+T = zeros(s(2),1);
 for i=1:s(2)
-    [f(:,i),thea(i),h_oneF(i,:)] = oneFstump(x(:,i),y,p);
-    T(i)=sum(abs(sign(h_oneF(i,:)-y')));
+[f(:,i),thea(i),h(:,i)] = oneFstump(data(:,i),y);
+T(i) = sum(abs(sign(h(:,i)-y)));
 end
 
 [m, I] = min(T);
-f_opt = I;
+f_opt = f(:,I);
 thea_opt = thea(I);
-h = transpose(h_oneF(I,:));
+y_opt = h(:,I);
+
